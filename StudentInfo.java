42bd7a144b909d4dfeab528ba109c12ef563f549
saipravalim06-max <saipravali.m06@gmail.com@example.com>
2026-01-11 22:55:31 +0530

    Initial Java program

---
+++ StudentInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Sai Pravali Motamarri");
+        System.out.println("Register Number: 23MIC0086");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
